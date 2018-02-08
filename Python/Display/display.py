a00d1f397a1c813b2154d6ec851f67b50e99f369
Alexander Pichler <alexander.pichler99@gmx.at>
2018-02-08 12:20:39 +0100

    Remove some unecessary comments from the display.py

--- Python/Display/display.py
+++ Python/Display/display.py
@@ -60,21 +60,10 @@ draw = ImageDraw.Draw(image)
 # Draw a white filled box to clear the image.
 draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
 
-# Draw some shapes.
-#draw.ellipse((2,2,22,22), outline=0, fill=255)
-#draw.rectangle((24,2,44,22), outline=0, fill=255)
-#draw.polygon([(46,22), (56,2), (66,22)], outline=0, fill=255)
-#draw.line((68,22,81,2), fill=0)
-#draw.line((68,2,81,22), fill=0)
-
 # Load default font.
 font = ImageFont.load_default()
 
-# Alternatively load a TTF font.
-# Some nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 8)
-
-# Write some text.
+# Refresh the ip's every second.
 while True:
 	disp.clear();
 	# Draw a white filled box to clear the image.
